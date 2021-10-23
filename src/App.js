aad0ea97ba7b2b79f44d25efafc5b5980a1d2a9c
yash <yaswanth.k23@gmail.com>
2021-10-23 20:39:32 +0530

    adding components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React from "react";
+import ExpeseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Lets get started</h2>
+      <ExpeseItem />
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ExpeseItem() {
+  return (
+    <div>
+      <div>October 23rd 2021</div>
+      <div>
+        <h2>Bike Insurance</h2>
+        <div>$350.23</div>
+      </div>
+    </div>
+  );
+}
+export default ExpeseItem;
