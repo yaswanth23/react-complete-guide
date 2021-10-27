1ba8060b588ab0a4262f3fabe7a3d4938aee39d1
yash <yaswanth.k23@gmail.com>
2021-10-27 22:43:24 +0530

    button missplace

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -17,9 +17,9 @@ const ExpenseForm = () => {
           <label>Date</label>
           <input type="date" min="2021-01-01" max="2022-12-31" />
         </div>
-        <div className="new-expense__actions">
-          <button type="submit">Add Expense</button>
-        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
       </div>
     </form>
   );
